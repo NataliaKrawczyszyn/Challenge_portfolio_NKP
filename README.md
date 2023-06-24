588247586e5fe2888027136af1a12aa330722dd6
NataliaKrawczyszyn <124452200+NataliaKrawczyszyn@users.noreply.github.com>
2023-06-24 19:39:32 +0200

    first_task

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# framework_test
+# **<h2>Dlaczego zdecydowałam się wziąć udział w wyzwaniu Dare IT Challenge?<h2>**
+
+# Polubiłam Pythona i testowanie manualne, dlaczego zatem nie połączyć dotychczasowej wiedzy w jedną ściężkę dalszego rozwoju - testowanie automatyczne w Pythonie? Testy automatyczne są przyszłością i chcę być częścią tej rewolucji, która już się rozpoczęła. Niech moc będzie z nami, uczestnikami!
--- test_cases/framework.py
+++ test_cases/framework.py
@@ -1,15 +1,16 @@
 import os
 import unittest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
-
 
 class Test(unittest.TestCase):
 
     @classmethod
     def setUp(self):
         os.chmod(DRIVER_PATH, 755)
-        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver_service = Service(executable_path=DRIVER_PATH)
+        self.driver = webdriver.Chrome(service=self.driver_service)
         self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
         self.driver.fullscreen_window()
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
@@ -23,3 +24,5 @@ class Test(unittest.TestCase):
 
     # Element of the first task: Try to search the Internet yourself how to get rid of the error:
     # "DeprecationWarning: executable_path has been deprecated, please pass in a Service object"
+
+
